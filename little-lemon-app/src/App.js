d721b681ef0148597a260658e20c554c9cfef60c
gjmessino <gabyjmessino@gmail.com>
2024-01-06 12:54:01 -0800

    added meta tags

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home Page</h1>
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    <Body/>
+      <main id="hero_section">
+        <article id= "highlights"></article>
+        <article id = "testimonials"></article>
+        <article id= "about"></article>
+      </main>
+    <Footer/>
+   </>
   );
 }
 
---
+++ little-lemon-app/src/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+    return (
+        <img></img>
+        <ul>
+            <li><a></a>Home</a></li>
+            <li><a></a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Log In</a>></li>
+        </ul>
+    )
+}
