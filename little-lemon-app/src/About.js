eaf0a261d376dd32582e71bba61e0aa9e4540c59
gjmessino <gabyjmessino@gmail.com>
2024-01-14 13:41:29 -0800

    Create About.js

---
+++ little-lemon-app/src/About.js
@@ -0,0 +1,16 @@
+function About(){
+    return(
+        <div>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+                d dfoijdf doidssdf <br/>
+                odisj fsd dksj doidj<br/>
+                jkdfnkjdfkjndsfj<br/>
+                ksdfkldsf
+            </p>
+            <img src = "" alt = "" />
+            <img src = "" alt = "" />
+        </div>
+    )
+}
