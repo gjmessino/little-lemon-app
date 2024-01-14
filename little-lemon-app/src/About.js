43973cc88a88c335b9ac01f8665b03ddb6a08707
gjmessino <gabyjmessino@gmail.com>
2024-01-14 15:55:10 -0800

    Update About.js

--- little-lemon-app/src/About.js
+++ little-lemon-app/src/About.js
@@ -1,13 +1,16 @@
 function About(){
     return(
-        <div>
-            <h1>Little Lemon</h1>
+        <div class = "container" id="about">
+            <h1 id = "title">Little Lemon</h1>
             <h2>Chicago</h2>
             <p>
-                d dfoijdf doidssdf <br/>
-                odisj fsd dksj doidj<br/>
-                jkdfnkjdfkjndsfj<br/>
-                ksdfkldsf
+                Little Lemon is a charming <br/>
+                neighborhood bistro that serves <br/>
+                simple food and classic <br/>
+                cocktails in a lively but casual<br/>
+                environment. The restuarant<br/>
+                features a locally sourced menu<br/>
+                with daily specials.
             </p>
             <img src = "./public/restaurant-chefB.jpg" alt = "Chef Cooking" />
             <img src = "./public/Mario&AdrianA.jpg" alt = "Two Chefs Talking" />
