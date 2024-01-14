4b338ff9d828c3ec3762eec17b35fab532acecb4
gjmessino <gabyjmessino@gmail.com>
2024-01-14 14:12:36 -0800

    Update Nav.js

--- little-lemon-app/src/Nav.js
+++ little-lemon-app/src/Nav.js
@@ -1,13 +1,12 @@
 function Nav() {
     return (
-        <img></img>
         <ul>
-            <li><a></a>Home</a></li>
-            <li><a></a></li>
+            <li><a>Home</a></li>
             <li><a>Menu</a></li>
             <li><a>Reservations</a></li>
             <li><a>Order Online</a></li>
-            <li><a>Log In</a>></li>
+            <li><a>Log In</a></li>
         </ul>
     )
 }
+export default Nav();
