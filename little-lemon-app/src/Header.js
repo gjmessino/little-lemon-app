e095d24c5e0f7bd5824efc7b5a07c58107c52828
gjmessino <gabyjmessino@gmail.com>
2024-01-14 13:07:20 -0800

    Update Header.js

---
+++ little-lemon-app/src/Header.js
@@ -0,0 +1,20 @@
+
+function Header() {
+    return (
+      <flexbox>
+        <img src="" alt="Little Lemon Logo"/>
+        <ul>
+            <ol>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Menu</a></li>
+                <li><a>Reservations</a></li>
+                <li><a>Order Online</a></li>
+                <li><a>Log In</a></li>
+            </ol>
+        </ul>
+      </flexbox>
+    );
+  }
+  
+  export default Header;
