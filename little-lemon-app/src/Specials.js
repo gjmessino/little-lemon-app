ec1cde92d090d13ed576718a2cd824b6c0be84f8
gjmessino <gabyjmessino@gmail.com>
2024-01-14 13:41:38 -0800

    Create Specials.js

---
+++ little-lemon-app/src/Specials.js
@@ -0,0 +1,11 @@
+function Specials(){
+    return(
+        <div>
+            <h1><b>This Week's Specials</b></h1>
+            <button>Online Menu</button>
+            <SpecialsCards/>
+            <SpecialsCards/>
+            <SpecialsCards/>
+        </div>
+    )
+}
