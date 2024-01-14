37d6bf2f0bc04e636256161dd47813670b59d8f5
gjmessino <gabyjmessino@gmail.com>
2024-01-14 14:12:12 -0800

    Update Footer.js

--- little-lemon-app/src/Footer.js
+++ little-lemon-app/src/Footer.js
@@ -1,30 +1,31 @@
 function Footer() {
     return(
-        <img></img>
-        <div>
-            <h1>Doormat Nav</h1>
-            <ul>
-                <li><a>About</a></li>
-                <li><a></a></li>
-                <li><a>Menu</a></li>
-                <li><a>Reservations</a></li>
-                <li><a>Order Online</a></li>
-                <li><a>Log In</a>></li>
-            </ul>
-        </div>
-        <div>
-            <h1>Contact</h1>
-            <ul>
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-            </ul>
-        </div>
-        <div>
-            <h1>Social Media</h1>
-            <img><a></a></img>
-            <img><a></a></img>
-            <img><a></a></img>
-        </div>
+        <footer>
+            <div>
+                <h1>Doormat Nav</h1>
+                <ul>
+                    <li><a>About</a></li>
+                    <li><a>Menu</a></li>
+                    <li><a>Reservations</a></li>
+                    <li><a>Order Online</a></li>
+                    <li><a>Log In</a></li>
+                </ul>
+            </div>
+            <div>
+                <h1>Contact</h1>
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+            <div>
+                <h1>Social Media</h1>
+                <img><a></a></img>
+                <img><a></a></img>
+                <img><a></a></img>
+            </div>
+        </footer>
     )
 }
+export default Footer();
