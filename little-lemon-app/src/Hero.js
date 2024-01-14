4eaf6672dbcef165f02a807a7b882b2ef8c9d76b
gjmessino <gabyjmessino@gmail.com>
2024-01-14 13:41:33 -0800

    Create Hero.js

---
+++ little-lemon-app/src/Hero.js
@@ -0,0 +1,16 @@
+function Hero(){
+    return(
+        <div>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family owned <br/>
+                Mediterranean restaurant, <br/>
+                focused on traditional <br/>
+                recipes served with a <br/>
+                modern twist
+            </p>
+            <button>Reserve a Table</button>
+            <img src = "" alt = "Bruschetta"/>
+        </div>
+    )
+}
