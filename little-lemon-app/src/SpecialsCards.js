2b6f4a1c24c3419551b75c5b8fe47552f2075864
gjmessino <gabyjmessino@gmail.com>
2024-02-11 15:18:55 -0800

    Update SpecialsCards.js

--- little-lemon-app/src/SpecialsCards.js
+++ little-lemon-app/src/SpecialsCards.js
@@ -1,10 +1,10 @@
-function SpecialsCards(data){
+function SpecialsCards(props){
     return (
         <div class = "container">
             <img src = "data.dishImg"/>
-            <h2 class = "card">${data.dish}</h2>
-            <h2>${data.price}</h2>
-            <p>${data.description}</p>
+            <h2 class = "card">props.dish</h2>
+            <h2>props.price</h2>
+            <p>props.description</p>
             <h2>Order for Delivery</h2>
             <img></img>
         </div>
