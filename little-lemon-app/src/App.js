86741d592cff12187689aad6723a79a8cd18a6d2
gjmessino <gabyjmessino@gmail.com>
2024-02-11 15:18:26 -0800

    Update App.js

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import Header from "./Header";
+import Hero from "./Hero";
+import Specials from "./Specials";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+// import { Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -7,8 +13,15 @@ function App() {
       <meta property="og:title" content="Little Lemon Restaurant" />
       <meta property="og:description" content="Little Lemon is a local Mediterranean restaurant focused on traditional recipe" />
       <meta property="og:image" content="" />
-
-      <Header />
+      {/* <BrowserRouter>
+        <Route path="/" element={<App/>}></Route>
+        <Route path="/menu" element = {<Menu/>}></Route>
+        <Route path="/booking" element={<ReservationPage/>}></Route>
+        <Route path="/order" element={<Order/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </BrowserRouter> */}
+      {/* <Nav/> */}
+      <Header/>
       <Hero/>
       <Specials/>
       <Testimonials/>
