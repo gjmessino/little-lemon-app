9ace62bff2ed0466fc1eea2eb88c9d96be5212bc
gjmessino <gabyjmessino@gmail.com>
2024-02-11 15:18:57 -0800

    Update Testimonials.js

--- little-lemon-app/src/Testimonials.js
+++ little-lemon-app/src/Testimonials.js
@@ -1,10 +1,17 @@
+import Reviews from "./Reviews"
 function Testimonials(){
     return(
         <div class = "container" id = "testimonials">
             <h1>Testimonials</h1>
-            <Reviews/>
-            <Reviews/>
-            <Reviews/>
+            <Reviews
+            userName = "g"
+            review = "f"/>
+            <Reviews
+             userName = "g"
+             review = "f"/>
+            <Reviews
+             userName = "f"
+             review = "g"/>
         </div>
     )
 }
